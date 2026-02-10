87f676b12e3f38e20bd4140eae8295596d1f8d59
pi-a <81693850+pi-a@users.noreply.github.com>
2026-02-10 15:22:25 +0000

    Deploying to gh-pages from @ pi-a/pi-a.github.io@65167eefaf2dfdc554ba3af148b110d84d4552c7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-curriculum-vitae",
           title: "Curriculum Vitae",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "To see more detail please refer to my [LinkedIn](https://www.linkedin.com/in/pajam-atufi/) page.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
